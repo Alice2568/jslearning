9f3fa51f32785620eee48b7d10673a9500af7787
Marissal Alice <alice.ternois@orange.fr>
2024-10-09 14:00:06 +0200

    majchap14end

--- exercice1.js
+++ exercice1.js
@@ -1,26 +1,24 @@
-
         
-        
-        document.getElementById("button1").addEventListener("click", verif())
         function verif(){
          
-         const nombreMagique = Math.floor(Math.random() * 100) + 1;
-        console.log(nombreMagique);
+         
         const userInput = document.getElementById("textBox1").value;
         
-            // Vérifier si la saisie est un nombre valide
-            
+            if (userInput!=nombreMagique){
+
                 
                 if (userInput > nombreMagique){
                     alert("Trop grand! ");
                 }
 
-                else if( userInput <nombreMagique){
-                    alert("Trop petit! ");
-            }
-
                 else {
-                    alert("Félicitations ! Vous avez trouvé le nombre magique !")
-                
+                    alert("Trop petit! ");
             }}
+
+            else {
+                    alert("Félicitations ! Vous avez trouvé le nombre magique !");
+                    
+                    
+            }
+                    }
    
